74cabb5aff601d3f08cb4512889bcffa5caa1439
Luis Rosal <luiss.rosalg.13@gmail.com>
2022-10-21 22:24:11 +0000

    testing commits to different branches

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,23 +4,43 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  const fetchFarmaciaAPI = async () => {
+    try {
+      const response = await fetch(
+        "http://www.farmaplus.com.ve/rest/products?instock=false&page=1&searchStr=dol&status=1"
+      );
+      const body = response.json;
+      console.log(body);
+      console.log("okay");
+      return body;
+    } catch (error) {}
+  };
+
+  return (
+    <div className="text-center mt-5">
+      <h1>Hello Rigo!!</h1>
+      <p>
+        <img src={rigoImageUrl} />
+      </p>
+      <div className="alert alert-info">
+        {store.message ||
+          "Loading message from the backend (make sure your python backend is running)..."}
+      </div>
+      <p>
+        This boilerplate comes with lots of documentation:{" "}
+        <a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
+          Read documentation
+        </a>
+      </p>
+      <button
+        type="button"
+        class="btn btn-primary"
+        onClick={(e) => fetchFarmaciaAPI()}
+      >
+        Primary
+      </button>
+    </div>
+  );
 };
