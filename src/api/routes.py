84165cb1a6daa7eabf9ca28226239a77f02836f5
Iskandar <seik747@gmail.com>
2022-10-24 17:11:15 +0000

    endpoint for login

--- src/api/routes.py
+++ src/api/routes.py
@@ -15,4 +15,26 @@ def handle_hello():
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
 
+@api.route('/login', methods=['POST'])
+def log_user():
+    body = request.json
+    user = User.query.filter_by(email = body["email"]).one_or_none()
+    if user is None:
+        return jsonify({
+            "msg": "Email or password invalid"
+        }), 400
+
+    valid_password = check_password_hash(user.password, f'{body["password"]}{user.salt}')
+    if not valid_password:
+        return jsonify({
+            "msg": "Email or password invalid"
+        }), 400
+
+    access_token = create_access_token(identity=user.id)
+    response_body = {
+        "msg": "Welcome back, " + user.username,
+        "id": user.id,
+        "token": access_token
+    }
+
     return jsonify(response_body), 200
