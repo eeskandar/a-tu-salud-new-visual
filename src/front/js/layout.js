525b62f67ff1991c5616307c94ab490d12a1a8f6
Manuelacostar97 <manuelacostar97@gmail.com>
2022-10-24 23:59:06 +0000

    Agregue login a layout

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,6 +5,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import { Login } from "./pages/login"
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
