b9222377f123d329faaa86abaafb237dc4e65530
Iskandar <seik747@gmail.com>
2022-10-24 17:04:08 +0000

    making the inputs functional

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -13,33 +13,33 @@ import { NavbarVertical } from "./component/NavbarVertical";
 
 //create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                <Navbar />
-                    <div className="row">
-                        <div className="col-2">
-                            <NavbarVertical />
-                        </div>
-                        <div className="col-10">    
-                            <Routes>
-                                <Route element={<Home />} path="/" />
-                                <Route element={<Demo />} path="/demo" />
-                                <Route element={<Single />} path="/single/:theid" />
-                                <Route element={<h1>Not found!</h1>} />
-                            </Routes>
-                        </div>
-					</div>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <div className="row">
+            <div className="col-2">
+              <NavbarVertical />
+            </div>
+            <div className="col-10">
+              <Routes>
+                <Route element={<Home />} path="/" />
+                <Route element={<Demo />} path="/demo" />
+                <Route element={<Single />} path="/single/:theid" />
+                <Route element={<h1>Not found!</h1>} />
+              </Routes>
+            </div>
+          </div>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../../styles/home.css";
 
 export const Login = () => {
   const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  async function login(email, password) {
+    try {
+      let user;
+      user = { email: email, password: password };
+      const response = await fetch(process.env.BACKEND_URL + "/login", {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        new Error("Ocurrió un error en la solicitud");
+      }
+      const body = await response.json();
+      localStorage.setItem("token", body.token);
+      navigate("/");
+    } catch (error) {}
+  }
 
   return (
     <div>
@@ -16,6 +40,10 @@ export const Login = () => {
             <input
               type="email"
               className="form-control form-input border-0"
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+              value={email}
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
             />
@@ -30,6 +58,10 @@ export const Login = () => {
             <input
               type="password"
               className="form-control form-input border-0"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+              value={password}
               id="exampleInputPassword1"
             />
           </div>
@@ -37,6 +69,11 @@ export const Login = () => {
             <button
               type="submit"
               className="btn form-btn text-center btn-lg rounded-pill px-5"
+              onClick={(e) => {
+                if (email.trim() != "" && password.trim() != "") {
+                  login(email, password);
+                }
+              }}
             >
               Iniciar sesión
             </button>
