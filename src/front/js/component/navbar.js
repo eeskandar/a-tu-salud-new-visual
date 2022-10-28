4d8bf9ffd0e67f7b56c38cab5b8ebcc35f395b9f
Iskandar <seik747@gmail.com>
2022-10-28 21:13:39 +0000

    polishing the navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -10,80 +10,73 @@ export const Navbar = () => {
   const logout = actions.logout;
 
   return (
-    <>
-      <nav className="navbar navbar-expand-sm py-0 bg-info text-white">
+    <div>
+      <nav className="navbar navbar-expand-sm py-0 text-white nav-color">
         <div className="container-fluid">
           <Link to="">
-            <div className="navbar-brand text-white" href="#">
-              <img src={logo} style={{ height: "50px" }}></img>
-            </div>
+            <img src={logo} className="nav-logo ms-3"></img>
           </Link>
           <div
             className="navbar d-flex justify-content-end"
             id="navbarSupportedContent"
           >
-            <form className="d-flex" role="search">
-              <h2 className="px-4">
-                <i className="fa-solid fa-magnifying-glass"></i>
-              </h2>
+            <form className="d-flex pe-5" role="search">
+              <i className="fa-solid fa-magnifying-glass fs-2 px-4"></i>
               <input
-                className="form-control me-2 bg-info  bg-opacity-25"
+                className="form-control me-2 nav-input border-0 rounded-pill"
                 type="search"
                 placeholder="Buscar"
                 aria-label="Search"
               />
             </form>
-            <div className="navbar " id="navbarNav">
-              <ul className="navbar-nav">
-                {localStorage.getItem("token") == null ? (
-                  <div className="d-flex">
-                    <li className="nav-item px-4">
-                      <a
-                        className="nav-link text-white"
-                        aria-current="page"
-                        href="#"
-                      >
-                        Registrarse
-                      </a>
-                    </li>
-                    <li className="nav-item px-4">
-                      <Link to="/login" className="nav-link text-white">
-                        Iniciar Sesión
-                      </Link>
-                    </li>
-                  </div>
-                ) : (
-                  <div className="d-flex">
-                    <li className="nav-item px-4">
-                      <Link
-                        to={`/user/${activeUser[0].id}`}
-                        className="nav-link text-white"
-                        aria-current="page"
-                      >
-                        Perfil de usuario
-                      </Link>
-                    </li>
-                    <li className="nav-item px-4">
-                      <Link
-                        to="/"
-                        className="nav-link text-white"
-                        onClick={(e) => {
-                          logout();
-                          navigate("/");
-                        }}
-                      >
-                        Cerrar sesión
-                      </Link>
-                    </li>
-                  </div>
-                )}
-              </ul>
-            </div>
+            <ul className="navbar-nav py-1">
+              {localStorage.getItem("token") == null ? (
+                <div className="d-flex">
+                  <li className="nav-item px-4">
+                    <Link
+                      to=""
+                      className="nav-link text-white"
+                      aria-current="page"
+                    >
+                      Registrarse
+                    </Link>
+                  </li>
+                  <li className="nav-item px-4">
+                    <Link to="/login" className="nav-link text-white">
+                      Iniciar Sesión
+                    </Link>
+                  </li>
+                </div>
+              ) : (
+                <div className="d-flex">
+                  <li className="nav-item px-4">
+                    <Link
+                      to={`/user/${activeUser[0].id}`}
+                      className="nav-link text-white"
+                      aria-current="page"
+                    >
+                      Perfil de usuario
+                    </Link>
+                  </li>
+                  <li className="nav-item px-4">
+                    <Link
+                      to="/"
+                      className="nav-link text-white"
+                      onClick={(e) => {
+                        logout();
+                        navigate("/");
+                      }}
+                    >
+                      Cerrar sesión
+                    </Link>
+                  </li>
+                </div>
+              )}
+            </ul>
           </div>
         </div>
       </nav>
-      <div style={{ background: "#92DCE5", height: "10px" }}></div>
-      <div></div>
-    </>
+      <div className="nav-bottom"></div>
+    </div>
   );
 };
