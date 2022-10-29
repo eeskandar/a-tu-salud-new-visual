5e1249db75fbc76e2a3c45b3e6c6a6803c2ca948
Luis Rosal <luiss.rosalg.13@gmail.com>
2022-10-29 18:42:31 +0000

    now side navbar is conditionally rendered

--- src/front/js/component/NavbarVertical.js
+++ src/front/js/component/NavbarVertical.js
@@ -3,30 +3,33 @@ import { Link } from "react-router-dom";
 
 export const NavbarVertical = () => {
   return (
-    <div className="bg-light container-fluid view-display my-auto padding-a">
-      <div className="">
-        <Link to="" className="nav-link text-secondary mb-4">
-          <i className="fa-solid fa-hand-holding-heart fs-3 pe-2"></i> Nueva
-          Donación
+    <div className="bg-light container-fluid view-display my-auto p-2">
+      <div className="p-lg-5">
+        <Link to="" className="d-flex nav-link text-secondary mb-4">
+          <i className="fa-solid fa-hand-holding-heart fs-3 pe-2"></i>{" "}
+          <span className="d-none d-lg-block">Nueva Donación</span>
         </Link>
-        <Link to="" className="nav-link text-secondary mb-4">
-          <i className="fa-regular fa-square-plus fs-3 pe-2"></i> Nueva Petición
+        <Link to="" className="d-flex nav-link text-secondary mb-4">
+          <i className="fa-regular fa-square-plus fs-3 pe-2"></i>
+          <span className="d-none d-lg-block">Nueva Petición</span>
         </Link>
-        <Link to="" className="nav-link text-secondary mb-4">
-          <i className="fa-solid fa-arrow-right-arrow-left fs-3 pe-2"></i> Nuevo
-          Intercambio
+        <Link to="" className="d-flex nav-link text-secondary mb-4">
+          <i className="fa-solid fa-arrow-right-arrow-left fs-3 pe-2"></i>
+          <span className="d-none d-lg-block">Nueva Intercambio</span>
         </Link>
-        <Link to="" className="nav-link text-secondary mb-4">
-          <i className="fa-solid fa-envelope fs-3 pe-2"></i> Mensajes
+        <Link to="" className="d-flex nav-link text-secondary mb-4">
+          <i className="fa-solid fa-envelope fs-3 pe-2"></i>
+          <span className="d-none d-lg-block">Mensajes</span>
         </Link>
-        <Link to="" className="nav-link text-secondary mb-4">
-          <i className="fa-solid fa-circle-question fs-3 pe-2"></i> Sobre
-          Nosotros
+        <Link to="" className="d-flex nav-link text-secondary mb-4">
+          <i className="fa-solid fa-circle-question fs-3 pe-2"></i>{" "}
+          <span className="d-none d-lg-block">Sobre Nosotros</span>
         </Link>
       </div>
-      <div className="pb-5 mb-5 align-to-bot">
-        <Link to="" className="nav-link text-secondary">
-          <i className="fa-solid fa-gear fs-3 pe-2"></i> Configuración
+      <div className="pb-5 mb-5 align-to-bot p-lg-5">
+        <Link to="" className="d-flex nav-link text-secondary">
+          <i className="fa-solid fa-gear fs-3 pe-2"></i>{" "}
+          <span className="d-none d-lg-block">Configuración</span>
         </Link>
       </div>
     </div>
