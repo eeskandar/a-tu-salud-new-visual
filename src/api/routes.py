3a24b0cf316da3a8bd80dfb98153d2761e91fef4
Manuelacostar97 <manuelacostar97@gmail.com>
2022-11-01 00:09:21 +0000

    Priemra version del backend para el post del medicamento que necesita el usuario

--- src/api/routes.py
+++ src/api/routes.py
@@ -99,3 +99,30 @@ def posts():
                 "msg":"no posts found"
             }), 404
 
+@api.route('/solicitud', methods=['GET', 'POST'])
+def handle_solicitud():
+    if request.method == 'GET':
+        get_request = Post_donation.query.all()
+
+        if get_request is None:
+            return jsonify({
+                "msg": "There are no requests yet!"
+            }), 400
+
+        request_list = list(map(lambda requests: requests.serialize(), get_request))
+
+        return jsonify(request_list), 200
+
+    body = request.json    
+    new_request = Post_donation.create(body)   
+
+    if type(new_request) == dict:   
+        return jsonify({
+            "msg": new_request["msg"]
+        }), new_request["status"]
+
+    response_body = {     
+        "user": new_request.serialize()
+    }
+    return jsonify(response_body), 200
+
