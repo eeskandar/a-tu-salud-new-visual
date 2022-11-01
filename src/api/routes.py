d0754184a71db778aac77132bdcce1241860412f
Iskandar <seik747@gmail.com>
2022-11-01 00:18:54 +0000

    defining the front and fixing model problems

--- src/api/routes.py
+++ src/api/routes.py
@@ -11,14 +11,6 @@ import bcrypt
 
 api = Blueprint('api', __name__)
 
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
 @api.route('/login', methods=['POST'])
 def log_user():
     body = request.json
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -11,6 +11,7 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { NavbarVertical } from "./component/NavbarVertical";
+import { TradingPost } from "./pages/trading-post";
 
 //create your first component
 const Layout = () => {
@@ -32,6 +33,7 @@ const Layout = () => {
                 <Routes>
                   <Route element={<Home />} path="/" />
                   <Route element={<Login />} path="/login" />
+                  <Route element={<TradingPost />} path="/post/trade" />
                   <Route element={<Demo />} path="/demo" />
                   <Route element={<Single />} path="/single/:theid" />
                   <Route element={<h1>Not found!</h1>} />
