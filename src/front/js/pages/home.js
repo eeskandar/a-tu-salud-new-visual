41dadd90bb941bea8629ae14b410e8324008a462
Luis Rosal <luiss.rosalg.13@gmail.com>
2022-11-09 23:12:42 +0000

    cambios de ultimo minuto

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -144,7 +144,7 @@ export const Home = () => {
           </select>
         </div>
         <Link
-          className="btn form-btn text-center text-white btn-lg rounded mb-5 mt-4 px-5"
+          className="btn form-btn text-center text-white btn-lg rounded-pill mb-5 mt-4 px-5"
           onClick={(e) => {
             handleSubmit();
           }}
@@ -157,7 +157,7 @@ export const Home = () => {
         <div className="btn-group pt-0 pb-3 ms-5">
           <button
             type="button"
-            className="btn btn-secondary rounded"
+            className="btn btn-secondary rounded-pill"
             data-bs-toggle=""
             aria-expanded=""
             onClick={(e) => busquedaFiltro()}
