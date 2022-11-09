1449188d1f610900ac5db0fb99e3ad8900be1c85
Iskandar <seik747@gmail.com>
2022-11-09 21:07:22 +0000

    adding capital stuff

--- src/front/js/component/CardDonations.js
+++ src/front/js/component/CardDonations.js
@@ -21,19 +21,21 @@ export const CardDonations = (props) => {
           )}
         </div>
         <div className="m-auto w-50 p-2">
-          <h5 className="mt-2 text-secondary">{props.title}</h5>
+          <h5 className="mt-2 text-secondary m_title">{props.title}</h5>
           <div className="m-1 d-flex justify-content-between">
             <div>
               <h6 className="text-secondary mb-0">Cantidad</h6>
-              <p className="text-secondary mb-0">{props.quantity}</p>
+              <p className="text-secondary mb-0 m_title">{props.quantity}</p>
             </div>
             <div>
               <h6 className="text-secondary mb-0">Fecha de vencimiento</h6>
-              <p className="text-secondary mb-0">{props.expiration_date}</p>
+              <p className="text-secondary mb-0 m_title">
+                {props.expiration_date}
+              </p>
             </div>
           </div>
           <div className="m-1">
-            <p className="text-secondary my-2">{props.description}</p>
+            <p className="text-secondary my-2 m_phara">{props.description}</p>
           </div>
         </div>
         <div className="m-auto p-2">
